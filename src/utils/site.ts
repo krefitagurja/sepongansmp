c30766a074a5e1c4b6cb9c4a4957cb759a59cd1b
krefitagurja <krefitagurja@gmail.com>
2025-07-14 11:26:32 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Sepongan Smp';
-export const desk = 'Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma';
-export const url = 'https://sepongansmp.pages.dev';
-export const terbit = '2025-06-26T00:01:03Z';
